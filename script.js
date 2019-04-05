09b91545f62e92d9ed937abbaca953f1dc06f97f
ousmane91 <contact@ousmangueye.fr>
2019-04-05 16:35:41 +0200

    exocorriger

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function Number(){
-var firstNumber = document.getElementById('firstNumber').value;
-var secondNumber = document.getElementById('secondNumber').value;
-var result = firstNumber % secondNumber;
-
-alert(result);
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  //Pour connaitre le reste de la division
+  var result = firstNumber % secondNumber;
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert('Veuillez saisir uniquement un nombre');
+  } else {
+    alert(result);
+  }
 }
