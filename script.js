a603a9be0a8bca21d421dff677851e593251f65c
ousmane91 <contact@ousmangueye.fr>
2019-04-05 12:25:09 +0200

    fini

---
+++ script.js
@@ -0,0 +1,7 @@
+function Number(){
+var firstNumber = document.getElementById('firstNumber').value;
+var secondNumber = document.getElementById('secondNumber').value;
+var result = firstNumber % secondNumber;
+
+alert(result);
+}
